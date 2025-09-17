305fd473e073c803f99850faeef7816553e023a4
saiyadav20 <23d41a0520@sriindu.ac.in>
2025-09-17 12:35:32 +0530

    Update main2.java

--- main2.java
+++ main2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class main1{
+public class main2{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
